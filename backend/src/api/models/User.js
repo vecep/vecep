00e79dcc62531c443c866c156f7f46598b2dc9bd
yodono <suunake@gmail.com>
2022-01-06 11:43:35 -0300

    Refactor models and schemas to a better structure

---
+++ backend/src/api/models/User.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+import { requiredArray } from '../utils/index.js';
+import ImageSchema from '../schemas/Image.js';
+
+const UserSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: false
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	roles: {
+		type: [String],
+		validate: requiredArray
+	},
+	picture: ImageSchema
+});
+
+const User = mongoose.model('Image', UserSchema);
+
+export default User;
